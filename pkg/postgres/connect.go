a01c00532efd52cf8e32dc69d10e161a76273682
RakhimovAns <ansarrakhimov91@gmail.com>
2025-08-06 23:30:48 +0500

    db

---
+++ pkg/postgres/connect.go
@@ -0,0 +1,169 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/defany/db/v2/pkg/retry"
+	"github.com/defany/slogger/pkg/logger/sl"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	defaultMaxConnAttempts = 3
+	defaultRetryConnDelay  = time.Second
+	defaultAcquireTimeout  = 5 * time.Second
+)
+
+type Config struct {
+	Username string
+	Password string
+	Host     string
+	Port     string
+	Database string
+
+	ConnAmount        *int32
+	MinConnAmount     *int32
+	MaxConnIdleTime   *time.Duration
+	MaxConnLifetime   *time.Duration
+	HealthCheckPeriod *time.Duration
+	AcquireTimeout    time.Duration
+
+	maxConnAttempts int
+	retryConnDelay  time.Duration
+
+	tracer pgx.QueryTracer
+}
+
+func NewConfig(username, password, host, port, database string) *Config {
+	return &Config{
+		Username: username,
+		Password: password,
+		Host:     host,
+		Port:     port,
+		Database: database,
+
+		maxConnAttempts: defaultMaxConnAttempts,
+		retryConnDelay:  defaultRetryConnDelay,
+
+		AcquireTimeout: defaultAcquireTimeout,
+	}
+}
+
+func (c *Config) WithConnAmount(amount int32) *Config {
+	c.ConnAmount = &amount
+	return c
+}
+
+func (c *Config) WithMinConnAmount(amount int32) *Config {
+	c.MinConnAmount = &amount
+	return c
+}
+
+func (c *Config) WithMaxConnIdleTime(d time.Duration) *Config {
+	c.MaxConnIdleTime = &d
+	return c
+}
+
+func (c *Config) WithMaxConnLifetime(d time.Duration) *Config {
+	c.MaxConnLifetime = &d
+	return c
+}
+
+func (c *Config) WithHealthCheckPeriod(d time.Duration) *Config {
+	c.HealthCheckPeriod = &d
+	return c
+}
+
+func (c *Config) WithAcquireTimeout(d time.Duration) *Config {
+	c.AcquireTimeout = d
+	return c
+}
+
+func (c *Config) WithMaxConnAttempts(attempts int) *Config {
+	c.maxConnAttempts = attempts
+	return c
+}
+
+func (c *Config) WithRetryConnDelay(delay time.Duration) *Config {
+	c.retryConnDelay = delay
+	return c
+}
+
+func (c *Config) WithTracer(tracer pgx.QueryTracer) *Config {
+	c.tracer = tracer
+	return c
+}
+
+func (c *Config) dsn() string {
+	return fmt.Sprintf(
+		"postgresql://%s:%s@%s:%s/%s",
+		c.Username, c.Password,
+		c.Host, c.Port, c.Database,
+	)
+}
+
+// NewClient создает и конфигурирует пул соединений.
+func NewClient(ctx context.Context, log *slog.Logger, cfg *Config) (pool *pgxpool.Pool, err error) {
+	dsn := cfg.dsn()
+
+	err = retry.WithAttempts(cfg.maxConnAttempts, cfg.retryConnDelay, func() error {
+		log.Info("connecting to postgresql database...")
+
+		connectCtx, cancel := context.WithTimeout(ctx, cfg.AcquireTimeout)
+		defer cancel()
+
+		pgxCfg, err := pgxpool.ParseConfig(dsn)
+		if err != nil {
+			log.Error("Unable to parse configs", sl.ErrAttr(err))
+			return err
+		}
+
+		if cfg.ConnAmount != nil {
+			pgxCfg.MaxConns = *cfg.ConnAmount
+		}
+
+		if cfg.MinConnAmount != nil {
+			pgxCfg.MinConns = *cfg.MinConnAmount
+		}
+
+		if cfg.MaxConnIdleTime != nil {
+			pgxCfg.MaxConnIdleTime = *cfg.MaxConnIdleTime
+		}
+
+		if cfg.MaxConnLifetime != nil {
+			pgxCfg.MaxConnLifetime = *cfg.MaxConnLifetime
+		}
+
+		if cfg.HealthCheckPeriod != nil {
+			pgxCfg.HealthCheckPeriod = *cfg.HealthCheckPeriod
+		}
+
+		pgxCfg.ConnConfig.Tracer = cfg.tracer
+
+		pool, err = pgxpool.NewWithConfig(connectCtx, pgxCfg)
+		if err != nil {
+			log.Error("failed to connect to postgres...", sl.ErrAttr(err))
+			return err
+		}
+
+		err = pool.Ping(ctx)
+		if err != nil {
+			log.Error("ping to postgres failed...", sl.ErrAttr(err))
+		}
+
+		return err
+	})
+
+	if err != nil {
+		log.Error("all attempts are exceeded. unable to connect to postgres database")
+		return nil, err
+	}
+
+	log.Info("connected to postgresql")
+	return pool, nil
+}
---
+++ pkg/postgres/queue/worker.go
@@ -0,0 +1,220 @@
+package queue
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/defany/db/v2/pkg/postgres"
+	slerr "github.com/defany/slogger/pkg/err"
+	"github.com/defany/slogger/pkg/logger/sl"
+	"github.com/gookit/goutil/arrutil"
+	"github.com/jackc/pgx/v5"
+	"github.com/riverqueue/river"
+	"github.com/riverqueue/river/rivertype"
+)
+
+var (
+	ErrJobIdsNotProvided = fmt.Errorf("job ids not provided")
+	ErrJobNotFound       = fmt.Errorf("job not found")
+)
+
+type Options struct {
+	ScheduledAt time.Time
+	MaxAttempts int
+	Metadata    []byte
+	Pending     bool
+	Priority    int
+	Queue       string
+	Tags        []string
+	UniqueOpts  river.UniqueOpts
+}
+
+type Worker[T river.JobArgs] interface {
+	Put(ctx context.Context, args T) (int64, error)
+	PutBatch(ctx context.Context, args ...T) ([]int64, error)
+	PutWithOpts(ctx context.Context, options Options, args T) (int64, error)
+	PutBatchWithOpts(ctx context.Context, options Options, args ...T) ([]int64, error)
+	JobStatuses(ctx context.Context, ids ...int64) ([]JobStatus, error)
+	FetchJobs(ctx context.Context, ids ...int64) ([]*rivertype.JobRow, error)
+	FetchJob(ctx context.Context, id int64) (*rivertype.JobRow, error)
+}
+
+type Repository[T river.JobArgs] struct {
+	river *river.Client[pgx.Tx]
+}
+
+func New[T river.JobArgs](river *river.Client[pgx.Tx]) *Repository[T] {
+	return &Repository[T]{river: river}
+}
+
+func (r *Repository[T]) Put(ctx context.Context, args T) (int64, error) {
+	tx, ok := postgres.ExtractTX(ctx)
+	if ok {
+		out, err := r.river.InsertTx(ctx, tx, args, nil)
+		if err != nil {
+			return 0, err
+		}
+
+		return out.Job.ID, nil
+	}
+
+	out, err := r.river.Insert(ctx, args, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	return out.Job.ID, nil
+}
+
+func (r *Repository[T]) PutBatch(ctx context.Context, args ...T) ([]int64, error) {
+	op := sl.FnName()
+
+	insertParams := make([]river.InsertManyParams, 0, len(args))
+	for _, arg := range args {
+		insertParams = append(insertParams, river.InsertManyParams{
+			Args: arg,
+		})
+	}
+
+	tx, ok := postgres.ExtractTX(ctx)
+	if ok {
+		out, err := r.river.InsertManyTx(ctx, tx, insertParams)
+		if err != nil {
+			return nil, sl.Err(op, err)
+		}
+
+		jobIds := arrutil.Map(out, func(input *rivertype.JobInsertResult) (target int64, find bool) {
+			return input.Job.ID, true
+		})
+
+		return jobIds, nil
+	}
+
+	out, err := r.river.InsertMany(ctx, insertParams)
+	if err != nil {
+		return nil, sl.Err(op, err)
+	}
+
+	jobIds := arrutil.Map(out, func(input *rivertype.JobInsertResult) (target int64, find bool) {
+		return input.Job.ID, true
+	})
+
+	return jobIds, nil
+}
+
+func (r *Repository[T]) PutWithOpts(ctx context.Context, options Options, args T) (int64, error) {
+	tx, ok := postgres.ExtractTX(ctx)
+	if ok {
+		out, err := r.river.InsertTx(ctx, tx, args, &river.InsertOpts{
+			ScheduledAt: options.ScheduledAt,
+		})
+		if err != nil {
+			return 0, err
+		}
+
+		return out.Job.ID, nil
+	}
+
+	out, err := r.river.Insert(ctx, args, &river.InsertOpts{
+		ScheduledAt: options.ScheduledAt,
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	return out.Job.ID, nil
+}
+
+func (r *Repository[T]) PutBatchWithOpts(ctx context.Context, options Options, args ...T) ([]int64, error) {
+	op := sl.FnName()
+
+	insertParams := make([]river.InsertManyParams, 0, len(args))
+	for _, arg := range args {
+		insertParams = append(insertParams, river.InsertManyParams{
+			Args: arg,
+			InsertOpts: &river.InsertOpts{
+				ScheduledAt: options.ScheduledAt,
+			},
+		})
+	}
+
+	tx, ok := postgres.ExtractTX(ctx)
+	if ok {
+		out, err := r.river.InsertManyTx(ctx, tx, insertParams)
+		if err != nil {
+			return nil, sl.Err(op, err)
+		}
+
+		jobIds := arrutil.Map(out, func(input *rivertype.JobInsertResult) (target int64, find bool) {
+			return input.Job.ID, true
+		})
+
+		return jobIds, nil
+	}
+
+	out, err := r.river.InsertMany(ctx, insertParams)
+	if err != nil {
+		return nil, sl.Err(op, err)
+	}
+
+	jobIds := arrutil.Map(out, func(input *rivertype.JobInsertResult) (target int64, find bool) {
+		return input.Job.ID, true
+	})
+
+	return jobIds, nil
+}
+
+func (r *Repository[T]) JobStatuses(ctx context.Context, ids ...int64) ([]JobStatus, error) {
+	if len(ids) == 0 {
+		return nil, ErrJobIdsNotProvided
+	}
+
+	params := river.NewJobListParams().IDs(ids...)
+
+	jobs, err := r.river.JobList(ctx, params)
+	if err != nil {
+		return nil, slerr.WithSource(err)
+	}
+
+	statuses := arrutil.Map(jobs.Jobs, func(input *rivertype.JobRow) (target JobStatus, find bool) {
+		return JobStatus{
+			JobID:  input.ID,
+			Status: input.State,
+		}, true
+	})
+
+	return statuses, nil
+}
+
+func (r *Repository[T]) FetchJobs(ctx context.Context, ids ...int64) ([]*rivertype.JobRow, error) {
+	if len(ids) == 0 {
+		return nil, ErrJobIdsNotProvided
+	}
+
+	params := river.NewJobListParams().IDs(ids...)
+
+	jobs, err := r.river.JobList(ctx, params)
+	if err != nil {
+		return nil, slerr.WithSource(err)
+	}
+
+	if jobs == nil {
+		return nil, nil
+	}
+
+	return jobs.Jobs, nil
+}
+
+func (r *Repository[T]) FetchJob(ctx context.Context, id int64) (*rivertype.JobRow, error) {
+	jobs, err := r.FetchJobs(ctx, id)
+	if err != nil {
+		return nil, slerr.WithSource(err)
+	}
+
+	if len(jobs) == 0 {
+		return nil, ErrJobNotFound
+	}
+
+	return jobs[0], nil
+}
