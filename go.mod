ced074d99161f278bcb93e2c0e0f1df37e1cef97
RakhimovAns <ansarrakhimov91@gmail.com>
2025-08-15 20:09:13 +0300

    updated postgres

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/RakhimovAns/txmananger
 go 1.24.4
 
 require (
+	github.com/RakhimovAns/logger v1.0.0
 	github.com/gookit/goutil v0.6.18
 	github.com/jackc/pgx/v5 v5.7.4
 	github.com/pkg/errors v0.9.1
@@ -13,9 +14,12 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/color v1.18.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/riverqueue/river/riverdriver v0.22.0 // indirect
@@ -30,6 +34,7 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ tx_manager/manager.go
@@ -0,0 +1,207 @@
+package txman
+
+import (
+	"context"
+	crand "crypto/rand"
+	"encoding/hex"
+	"errors"
+	errs "errors"
+	"fmt"
+	mrand "math/rand"
+	"time"
+
+	slerr "github.com/RakhimovAns/logger/pkg/err"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	pkgerrors "github.com/pkg/errors"
+)
+
+const baseBackoff = 10 * time.Millisecond
+
+type Tx interface {
+	Commit(ctx context.Context) error
+	Rollback(ctx context.Context) error
+	Conn() *pgx.Conn
+	CopyFrom(ctx context.Context, table pgx.Identifier, cols []string, src pgx.CopyFromSource) (int64, error)
+	SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults
+	LargeObjects() pgx.LargeObjects
+	Prepare(ctx context.Context, name, sql string) (*pgconn.StatementDescription, error)
+	Exec(ctx context.Context, sql string, args ...any) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+	Begin(ctx context.Context) (pgx.Tx, error)
+}
+
+type TxManager interface {
+	ReadCommitted(ctx context.Context, h Handler, opts ...TxOption) error
+	RepeatableRead(ctx context.Context, h Handler, opts ...TxOption) error
+	Serializable(ctx context.Context, h Handler, opts ...TxOption) error
+	RunWithOpts(ctx context.Context, h Handler, opts []TxOption) error
+}
+
+type Postgres interface {
+	BeginTx(ctx context.Context, opts pgx.TxOptions) (Tx, error)
+}
+
+type Handler = func(context.Context) error
+type GenericHandler[T any] func(context.Context) (T, error)
+
+type TxOption func(*TxConfig)
+
+func WithRetry(n uint) TxOption               { return func(c *TxConfig) { c.Retry = n } }
+func WithIso(lvl pgx.TxIsoLevel) TxOption     { return func(c *TxConfig) { c.IsoLevel = lvl } }
+func ReadOnly(on bool) TxOption               { return func(c *TxConfig) { c.ReadOnly = on } }
+func WithMaxBackoff(d time.Duration) TxOption { return func(c *TxConfig) { c.MaxBackoff = d } }
+
+func New(db Postgres) TxManager {
+	return &txManager{db: db}
+}
+
+func ReadCommitted[T any](ctx context.Context, tm TxManager, h GenericHandler[T], opts ...TxOption) (T, error) {
+	opts = append(opts, WithIso(pgx.ReadCommitted))
+	return execTx(ctx, tm, h, opts...)
+}
+
+func RepeatableRead[T any](ctx context.Context, tm TxManager, h GenericHandler[T], opts ...TxOption) (T, error) {
+	opts = append(opts, WithIso(pgx.RepeatableRead))
+	return execTx(ctx, tm, h, opts...)
+}
+
+func Serializable[T any](ctx context.Context, tm TxManager, h GenericHandler[T], opts ...TxOption) (T, error) {
+	opts = append(opts, WithIso(pgx.Serializable))
+	return execTx(ctx, tm, h, opts...)
+}
+
+func InjectTX(ctx context.Context, tx Tx) context.Context {
+	ctx = context.WithValue(ctx, txKey{}, tx)
+	return context.WithValue(ctx, txQueryKey{}, generateShortID())
+}
+
+func ExtractTX(ctx context.Context) (Tx, bool) {
+	tx, ok := ctx.Value(txKey{}).(Tx)
+	return tx, ok
+}
+
+func ExtractTxQueryKey(ctx context.Context) (string, bool) {
+	k, ok := ctx.Value(txQueryKey{}).(string)
+	return k, ok
+}
+
+type txManager struct {
+	db Postgres
+}
+
+type TxConfig struct {
+	IsoLevel   pgx.TxIsoLevel
+	Retry      uint
+	ReadOnly   bool
+	MaxBackoff time.Duration
+}
+
+func (tm *txManager) ReadCommitted(ctx context.Context, h Handler, opts ...TxOption) error {
+	opts = append(opts, WithIso(pgx.ReadCommitted))
+	return tm.RunWithOpts(ctx, h, opts)
+}
+
+func (tm *txManager) RepeatableRead(ctx context.Context, h Handler, opts ...TxOption) error {
+	opts = append(opts, WithIso(pgx.RepeatableRead))
+	return tm.RunWithOpts(ctx, h, opts)
+}
+
+func (tm *txManager) Serializable(ctx context.Context, h Handler, opts ...TxOption) error {
+	opts = append(opts, WithIso(pgx.Serializable))
+	return tm.RunWithOpts(ctx, h, opts)
+}
+
+func (tm *txManager) RunWithOpts(ctx context.Context, h Handler, opts []TxOption) error {
+	cfg := TxConfig{}
+	for _, o := range opts {
+		o(&cfg)
+	}
+	return tm.run(ctx, cfg, h)
+}
+
+func (tm *txManager) run(ctx context.Context, cfg TxConfig, h Handler) error {
+	for attempt := uint(0); attempt <= cfg.Retry; attempt++ {
+		err := tm.execTx(ctx, cfg, h)
+		if err == nil {
+			return nil
+		}
+
+		var pgErr *pgconn.PgError
+		if !(errors.As(err, &pgErr) && (pgErr.Code == "40001" || pgErr.Code == "40P01")) {
+			return slerr.WithSource(err)
+		}
+
+		if attempt < cfg.Retry {
+			backoff := baseBackoff << attempt
+			if cfg.MaxBackoff > 0 && backoff > cfg.MaxBackoff {
+				backoff = cfg.MaxBackoff
+			}
+			sleep := backoff/2 + time.Duration(mrand.Int63n(int64(backoff/2)))
+			time.Sleep(sleep)
+		}
+	}
+
+	return fmt.Errorf("retries exceeded")
+}
+
+func (tm *txManager) execTx(ctx context.Context, cfg TxConfig, h Handler) (err error) {
+	if _, ok := ExtractTX(ctx); ok {
+		return h(ctx)
+	}
+
+	opts := pgx.TxOptions{IsoLevel: cfg.IsoLevel, AccessMode: chooseAccessMode(cfg.ReadOnly)}
+	tx, err := tm.db.BeginTx(ctx, opts)
+	if err != nil {
+		return pkgerrors.Wrap(err, "begin tx")
+	}
+
+	ctx = InjectTX(ctx, tx)
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("panic recovered: %w -> %v", err, r)
+		}
+		if err != nil {
+			if rbErr := tx.Rollback(ctx); rbErr != nil {
+				err = errs.Join(rbErr, err)
+			}
+			return
+		}
+		err = tx.Commit(ctx)
+	}()
+
+	return h(ctx)
+}
+
+func execTx[T any](ctx context.Context, tm TxManager, handler GenericHandler[T], opts ...TxOption) (out T, err error) {
+	cfg := TxConfig{IsoLevel: pgx.ReadCommitted}
+	for _, opt := range opts {
+		opt(&cfg)
+	}
+
+	err = tm.RunWithOpts(ctx, func(txCtx context.Context) error {
+		out, err = handler(txCtx)
+		return err
+	}, opts)
+
+	return
+}
+
+func chooseAccessMode(ro bool) pgx.TxAccessMode {
+	if ro {
+		return pgx.ReadOnly
+	}
+	return pgx.ReadWrite
+}
+
+func generateShortID() string {
+	var b [4]byte
+	if _, err := crand.Read(b[:]); err != nil {
+		return "errid"
+	}
+	return hex.EncodeToString(b[:])
+}
+
+type txKey struct{}
+type txQueryKey struct{}
